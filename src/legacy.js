8ae628722a11f92d0175d2cb892b6b672621fb4f
asmithdev <austin@asmithdev.com>
2019-03-09 13:00:47 -0500

    organize structure

--- src/legacy.js
+++ src/legacy.js
@@ -11,30 +11,19 @@
 
 'use strict';
 
-export default class legacy {
-
-  /**
-  * @constructor
-  * @function constructor - Sets properties for this class
-  */
-  constructor() {
-    this.run = this.legacyScaffold;
-  }
-
-  /**
-  * @function legacyScaffold - Provides library functionality for legacy devices
-  */
-  legacyScaffold(params, resolve) {
-    setTimeout(() => {
-      if(typeof self === 'undefined') {
-        var self = (global || window || this);
-      }
-      self.params = params;
-      self.rtn = {
-        data: []
-      };
-      params.hamstersJob();
-      resolve(rtn);
-    }, 4); //4ms delay (HTML5 spec minimum), simulate threading
-  }
-};
+/**
+* @function legacyScaffold - Provides library functionality for legacy devices
+*/
+export function legacyScaffold(params, resolve) {
+  setTimeout(() => {
+    if(typeof self === 'undefined') {
+      var self = (global || window || this);
+    }
+    self.params = params;
+    self.rtn = {
+      data: []
+    };
+    params.hamstersJob();
+    resolve(rtn);
+  }, 4); //4ms delay (HTML5 spec minimum), simulate threading
+}
